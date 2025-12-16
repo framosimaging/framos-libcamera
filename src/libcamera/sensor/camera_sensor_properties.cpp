4e49e1ff50ffa5255496d7aa730a33956a890b61
Dominik Bajec <d.bajec@framos.com>
2025-12-16 14:38:28 +0100

    Add Framos camera_helpers, isp files and sensor properties
    Signed-off-by: Dominik Bajec <d.bajec@framos.com>

--- src/libcamera/sensor/camera_sensor_properties.cpp
+++ src/libcamera/sensor/camera_sensor_properties.cpp
@@ -269,6 +269,36 @@ const CameraSensorProperties *CameraSensorProperties::get(const std::string &sen
 				.hblankDelay = 3
 			},
 		} },
+		{ "fr_imx662", {
+			.unitCellSize = { 2900, 2900 },
+			.testPatternModes = {},
+			.sensorDelays = {
+				.exposureDelay = 2,
+				.gainDelay = 2,
+				.vblankDelay = 2,
+				.hblankDelay = 2
+			},
+		} },
+		{ "fr_imx676", {
+			.unitCellSize = { 2000, 2000 },
+			.testPatternModes = {},
+			.sensorDelays = {
+				.exposureDelay = 2,
+				.gainDelay = 2,
+				.vblankDelay = 2,
+				.hblankDelay = 2
+			},
+		} },
+		{ "fr_imx678", {
+			.unitCellSize = { 2000, 2000 },
+			.testPatternModes = {},
+			.sensorDelays = {
+				.exposureDelay = 2,
+				.gainDelay = 2,
+				.vblankDelay = 2,
+				.hblankDelay = 2
+			},
+		} },
 		{ "imx519", {
 			.unitCellSize = { 1220, 1220 },
 			.testPatternModes = {
@@ -301,6 +331,16 @@ const CameraSensorProperties *CameraSensorProperties::get(const std::string &sen
 				.gainDelay = 2,
 				.vblankDelay = 3,
 				.hblankDelay = 3
+			},
+		} },
+		{ "fr_imx900", {
+			.unitCellSize = { 2250, 2250 },
+			.testPatternModes = {},
+			.sensorDelays = {
+				.exposureDelay = 2,
+				.gainDelay = 2,
+				.vblankDelay = 2,
+				.hblankDelay = 2
 			},
 		} },
 		{ "ov2685", {
